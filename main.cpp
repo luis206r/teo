3000443b77005ece005101e6d032303b67887452
luis206r <70814892+luis206r@users.noreply.github.com>
2021-11-11 00:49:05 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,29 +1,37 @@
-#include"libs.h"
-#include"afd.h"
+#include "libs.h"
+#include "afd.h"
 using namespace std;
 
-int main() {
-    AFD *automata =new AFD();
-    int n,is,nfs = 0;
-    cin>>n>>is>>nfs;
+int main()
+{
+    AFD *automata = new AFD();
+    int n, is, nfs = 0;
+    cin >> n >> is >> nfs;
     automata = new AFD(n, is, nfs);
-    for(int i=0;i<nfs;i++){
-        cin>>automata->fss[i];
+    for (int i = 0; i < nfs; i++)
+    {
+        cin >> automata->fss[i];
     }
-     
-    int a,b; 
-    int size =2*n, count = 0;
-    for(int i=0; i<size;i++){
-        if(i%2==0) cin>>n>>is>>a;
-        else{
-            cin>>n>>is>>b;
+
+    int a, b;
+    int size = 2 * n, count = 0;
+    for (int i = 0; i < size; i++)
+    {
+        if (i % 2 == 0)
+            cin >> n >> is >> a;
+        else
+        {
+            cin >> n >> is >> b;
             //cout<<"{\na:"<<a<<", b:"<<b<<"}\n";
-            (automata->ft)[count] = make_pair(a,b);
+            (automata->ft)[count] = make_pair(a, b);
             count++;
         }
     }
     automata->printft();
     string t = automata->convert_re();
-    cout<<endl<<t<<endl;
+    cout << "Expresion regular: " << endl
+         << endl
+         << t << endl
+         << endl;
     return 0;
 }
